f6170cc67dcd7fb26708b7b1060562aeca343bce
liammillsap9-prog <liammillsap9@gmail.com>
2026-07-06 02:34:21 -0600

    Update bot activity name in bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "I coded this as a  average person",
         // Activity type number (0 = Playing).
         type: 0,
       },
